15762b238b2d251dad3d5ddf489d8ee14a8c63a1
linh.nguyen5 <linh.nguyen5@CL-LINHNGUYEN5>
2018-04-04 17:49:59 +0700

    create controller

---
+++ MyEpiserverSite/Controllers/StartController.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace MyEpiserverSite.Controllers
+{
+    public class StartController : PageController<Models.Pages.StartPage>
+    {
+        // GET: Start
+        public ActionResult Index(StartPage startPage)
+        {
+            return View(startPage);
+        }
+    }
+}
---
+++ MyEpiserverSite/Models/Pages/StartPage.cs
@@ -0,0 +1,12 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using System;
+
+namespace MyEpiserverSite.Models.Pages
+{
+    [ContentType(Description = "My first start page", DisplayName = "EpiStartPage")]
+    public class StartPage : PageData
+    {
+
+    }
+}
